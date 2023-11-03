543993d7719a40ef29408ba82f69b1e2152ecfca
maciu665 <maciej.majchrzak665@gmail.com>
2023-11-03 17:41:02 +0100

    modified:   dcm-vtk-png.py 	modified:   legend3d.py

--- dcm-vtk-png.py
+++ dcm-vtk-png.py
@@ -1,9 +1,16 @@
 # importy
+
+"""
+A script for converting DICOM format data from CT scans into images. 
+The use of the VTK (Visualization Toolkit) library makes it possible to
+select any color scale and also to obtain additional data in the form of,
+for example, a legend containing three-dimensional visualizations of the converted data.
+
+"""
+
 import vtk
-import sys
 import os
 import numpy
-import cv2
 from vtk.util.numpy_support import vtk_to_numpy
 from PIL import Image
 import argparse
@@ -16,66 +23,97 @@ required.add_argument('-i','--input', required=True, type=str, help="input direc
 required.add_argument('-o','--output', required=True, type=str, help="output directory")
 required.add_argument('-c','--center', required=True, type=int, help="window center")
 required.add_argument('-w','--width', required=True, type=int, help="window width")
-optional.add_argument('-p',"--palette",type=str,help="color palette")
+optional.add_argument('-p',"--palette",type=str,help="color palette, bw - black-white (default), br - blue-red rainbow, cw - cool-warm",default="bw")
 
-args = parser.parse_args(["-i/home/maciejm/PHD/PUBLIKACJA_03/DCM/15/","-o/home/maciejm/PHD/PUBLIKACJA_03/IMG","-c100","-w200"])
+# args = parser.parse_args(["-i/home/maciejm/PHD/PUBLIKACJA_03/DCM/15/","-o/home/maciejm/PHD/PUBLIKACJA_03/IMG","-c100","-w100"])
+# args = parser.parse_args(["-i/home/maciejm/PHD/PUBLIKACJA_03/DCM/15/","-oIMG","-c100","-w200"])
+# args = parser.parse_args(["-i/home/maciejm/PHD/PUBLIKACJA_03/DCM/00000003/","-o/home/maciejm/PHD/PUBLIKACJA_03/IMG","-c100","-w500"])
+args = parser.parse_args()
 
-print(args.center)
+# creating the output directory
+output_directory = os.path.abspath(args.output)
+os.makedirs(output_directory, exist_ok=True)
 
 # conversion from c and w to min value and max value
 value_range_min = args.center - args.width/2
 value_range_max = args.center + args.width/2
 
-#dicom reader
+# dicom reader
 dicom_reader = vtk.vtkDICOMImageReader()
 dicom_reader.SetDirectoryName(args.input)
 dicom_reader.Update()
 dicom_data = dicom_reader.GetOutput()
 dicom_dims = dicom_data.GetDimensions()
-#print(dicom_dims)
-# print(dicom_data)
+dicom_bounds = dicom_data.GetBounds()
+dicom_spacing = list(dicom_data.GetSpacing())
 
-legend3d(dicom_data)
+# dicom data information
+print("\ndicom data resolution   : ",dicom_dims)
+print("dicom data spacing      : ",dicom_spacing)
+print("dicom data X dimensions : ",dicom_bounds[0],"-",dicom_bounds[1])
+print("dicom data Y dimensions : ",dicom_bounds[2],"-",dicom_bounds[3])
+print("dicom data Z dimensions : ",dicom_bounds[4],"-",dicom_bounds[5])
 
+# creating an image with 3d representation of dicom data
+legend3d(dicom_data,output_directory)
 
-#lookup_table = vtk.vtkLookupTable()
-#lookup_table.SetNumberOfTableValues(256)
-#lookup_table.SetHueRange(0.667, 0.0)
-# lookup_table.SetValueRange(0,256)
-#lookup_table.Build()
-
+# vtk color transfer function
 transfer_function = vtk.vtkColorTransferFunction()
 transfer_function.SetColorSpaceToHSV()
 transfer_function.HSVWrapOff()
-transfer_function.AddRGBSegment(value_range_min-1, 0, 0, 0, value_range_min, 0, 0, 0)
-transfer_function.AddRGBSegment(value_range_max, 1, 1, 1, value_range_max+1, 1, 1, 1)
-# transfer_function.AddRGBSegment(value_range_min, 0, 0, 1, value_range_max, 1, 0, 0)
+if args.palette == "cw":    # cold-warm
+    transfer_function.AddRGBSegment(value_range_min, 0, 0, 1, (value_range_max+value_range_min)/2, 1, 1, 1)
+    transfer_function.AddRGBSegment((value_range_max+value_range_min)/2, 1, 1, 1, value_range_max, 1, 0, 0)
+elif args.palette == "br":  # blue-red
+    transfer_function.AddRGBSegment(value_range_min, 0, 0, 1, value_range_max, 1, 0, 0)
+else:   # black-white
+    transfer_function.AddRGBSegment(value_range_min, 0, 0, 0, value_range_max, 1, 1, 1)
 transfer_function.Build()
 
-
-
+# conversion from dicom data values to RGB color
 coloring = vtk.vtkImageMapToColors()
-# coloring.SetLookupTable(lookup_table)
 coloring.SetLookupTable(transfer_function)
 coloring.SetOutputFormatToRGB()
 coloring.SetInputData(dicom_data)
 coloring.Update()
-
 color_data = coloring.GetOutput()
+
+# conversion from vtk array to numpy array and numpy array reshaping
 numpy_data = vtk_to_numpy(color_data.GetPointData().GetArray("DICOMImage"))
-numpy_data_reshaped = numpy_data.reshape((dicom_dims[2],dicom_dims[0],dicom_dims[1],3))
+numpy_data_reshaped = numpy_data.reshape((dicom_dims[2],dicom_dims[1],dicom_dims[0],3))
 
-#cv2.imwrite(os.path.join(ydir,"slice_%s.png"%(str(i).zfill(3))), dicom_np[:,:,i,:])
+# scales of nonuniform axes
+scales = [round((x/min(dicom_spacing)),5) for x in dicom_spacing]
 
-cv2_data_z = cv2.cvtColor(numpy.flip(numpy_data_reshaped[100,:,:,:],axis=0),cv2.COLOR_BGR2RGB)
-cv2.imwrite("obraz.png", cv2_data_z)
+# saving images in X axis (Y-Z plane)
+x_directory = os.path.join(output_directory,"X")
+os.makedirs(x_directory, exist_ok=True)
+for i in range(numpy_data_reshaped.shape[1]):
+    pil_data = Image.fromarray(numpy.flip(numpy_data_reshaped[:,i,:,:],axis=1))
+    if max(scales[1],scales[2]) > 1:
+        pil_data = pil_data.resize((int(round(pil_data.size[0]*scales[1],0)), int(round(pil_data.size[1]*scales[2],0))), Image.Resampling.LANCZOS)
+    pil_data.save(os.path.join(x_directory,"slice_%s.png"%(str(i).zfill(4))))
+print("\nsaved %i images from X axis"%numpy_data_reshaped.shape[1])
 
-pil_data = Image.fromarray(numpy.flip(numpy_data_reshaped[100,:,:,:],axis=0))
-pil_data.save("pil.png")
+# saving images in Y axis (X-Z plane)
+y_directory = os.path.join(output_directory,"Y")
+os.makedirs(y_directory, exist_ok=True)
+for i in range(numpy_data_reshaped.shape[2]):
+    pil_data = Image.fromarray(numpy.flip(numpy_data_reshaped[:,:,i,:],axis=1))
+    if max(scales[0],scales[2]) > 1:
+        pil_data = pil_data.resize((int(round(pil_data.size[0]*scales[0],0)), int(round(pil_data.size[1]*scales[2],0))), Image.Resampling.LANCZOS)
+    pil_data.save(os.path.join(y_directory,"slice_%s.png"%(str(i).zfill(4))))
+print("saved %i images from Y axis"%numpy_data_reshaped.shape[2])
 
-sys.exit()
-for i in range(512):
-    #cv2.imwrite(os.path.join(xdir,"slice_%s.png"%(str(i).zfill(3))), dicom_np[:,i,:,:])
-    #cv2.imwrite(os.path.join(ydir,"slice_%s.png"%(str(i).zfill(3))), dicom_np[:,:,i,:])
-    if i < z_layers:
-        cv2.imwrite(os.path.join(zdir,"slice_%s.png"%(str(z_layers-i).zfill(3))), numpy.flip(dicom_np[i,:,:,:],axis=0))
+# saving images in Z axis (X-Y plane)
+z_directory = os.path.join(output_directory,"Z")
+os.makedirs(z_directory, exist_ok=True)
+for i in range(numpy_data_reshaped.shape[0]):
+    pil_data = Image.fromarray(numpy.flip(numpy.flip(numpy_data_reshaped[i,:,:,:],axis=0),axis=1))
+    if max(scales[0],scales[1]) > 1:
+        pil_data = pil_data.resize((int(round(pil_data.size[0]*scales[0],0)), int(round(pil_data.size[1]*scales[1],0))), Image.Resampling.LANCZOS)
+    pil_data.save(os.path.join(z_directory,"slice_%s.png"%(str(i).zfill(4))))
+print("saved %i images from Z axis"%numpy_data_reshaped.shape[0])
+
+#cv2_data_z = cv2.cvtColor(numpy.flip(numpy_data_reshaped[100,:,:,:],axis=0),cv2.COLOR_BGR2RGB)
+#cv2.imwrite("obraz.png", cv2_data_z)
--- legend3d.py
+++ legend3d.py
@@ -1,34 +1,53 @@
 import vtk
+import numpy
+import os
+from PIL import Image, ImageDraw, ImageFont
+from vtk.util.numpy_support import vtk_to_numpy
 
+def window2pil_image(window):
+    w2if = vtk.vtkWindowToImageFilter()
+    w2if.SetInput(window)
+    w2if.Update()
+    numpy_image_data = vtk_to_numpy(w2if.GetOutput().GetPointData().GetArray("ImageScalars"))
+    numpy_image_data_reshaped = numpy_image_data.reshape((window.GetSize()[1], window.GetSize()[0],3))
+    return Image.fromarray(numpy.flip(numpy_image_data_reshaped,axis=0))
 
-def legend3d(dicom_data):
-    pcut = vtk.vtkContourFilter()
-    pcut.SetInputData(dicom_data)
+def legend3d(dicom_data,output_directory):
+    # contour filter for typical bone density
+    contour = vtk.vtkContourFilter()
+    contour.SetInputData(dicom_data)
     dicom_data.GetPointData().SetActiveScalars("DICOMImage")
-    pcut.SetValue(0,1000)
-    pcut.Update()
-    print(pcut.GetOutput())
+    contour.SetValue(0,500)
+    contour.Update()
 
-    renderer = vtk.vtkRenderer()
-    # window = vtkwin()
+    # dicom data spatial information
+    data_bounds = list(dicom_data.GetBounds())
+    data_center = list(dicom_data.GetCenter())
+    data_size_x, data_size_y, data_size_z = data_bounds[1]-data_bounds[0], data_bounds[3]-data_bounds[2], data_bounds[5]-data_bounds[4]
+
+    # vtk window and renderer
     window = vtk.vtkRenderWindow()
+    renderer = vtk.vtkRenderer()
     window.AddRenderer(renderer)
-    window.SetSize(1000,1000)
+    window.SetSize(640,480)
     window.OffScreenRenderingOn()
 
+    # background
     renderer.GradientBackgroundOff()
     renderer.SetBackground(0,0,0)
 
+    # vtk mapper
     mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(pcut.GetOutputPort())
+    mapper.SetInputConnection(contour.GetOutputPort())
     mapper.ScalarVisibilityOff()
 
+    # vtk actor
     actor = vtk.vtkActor()
     actor.SetMapper(mapper)
     renderer.AddActor(actor)
+    camera = renderer.GetActiveCamera()
 
-    ########################################################################	LIGHT
-
+    # lights
     lightKit = vtk.vtkLightKit()
     lightKit.MaintainLuminanceOff()
     lightKit.SetKeyLightIntensity(1)
@@ -37,21 +56,54 @@ def legend3d(dicom_data):
     lightKit.SetHeadLightWarmth(0.5)
     lightKit.SetKeyToFillRatio(2)
     lightKit.SetKeyToHeadRatio(4)
-    #lightKit.SetKeyToBackRatio(1000.1)
     lightKit.SetKeyLightAzimuth(30)
     lightKit.SetKeyLightElevation(45)
     lights = renderer.GetLights()
     lights.InitTraversal()
     lightKit.AddLightsToRenderer(renderer)
 
+    # camera focal point in the middle of data
+    camera.SetFocalPoint(data_center)
+    camera.SetParallelProjection(True)
+
+    # isometric view rendered to pil image
+    max_data_size = max(data_size_x,data_size_y,data_size_z)
+    camera.SetPosition([sum(x) for x in zip(data_center, [max_data_size,max_data_size,-max_data_size])])
+    camera.SetViewUp([0,0,-1])
+    camera.SetParallelScale(max_data_size/1.732)
     window.Render()
+    pil_image_iso = window2pil_image(window)
 
-    w2if = vtk.vtkWindowToImageFilter()
-    w2if.SetInput(window)
-    w2if.Update()
+    # view in X+ direction rendered to pil image
+    camera.SetPosition([sum(x) for x in zip(data_center, [data_size_x,0,0])])
+    camera.SetParallelScale(max(data_size_y,data_size_z)/2)
+    window.Render()
+    pil_image_x = window2pil_image(window)
 
-    image_writer = vtk.vtkPNGWriter()
-    image_writer.SetFileName("legend.ppng")
-    image_writer.SetInputData(w2if.GetOutput())
-    image_writer.Write()
+    # view in Y+ direction rendered to pil image
+    camera.SetPosition([sum(x) for x in zip(data_center, [0,data_size_y,0])])
+    camera.SetParallelScale(max(data_size_x,data_size_z)/2)
+    window.Render()
+    pil_image_y = window2pil_image(window)
 
+    # view in Z+ direction rendered to pil image
+    camera.SetPosition([sum(x) for x in zip(data_center, [0,0,-data_size_z])])
+    camera.SetViewUp([-1,0,0])
+    camera.SetParallelScale(max(data_size_y,data_size_x)/2)
+    window.Render()
+    pil_image_z = window2pil_image(window)
+
+    # merging images
+    legend = Image.new("RGB", (window.GetSize()[0]*2, window.GetSize()[1]*2), (0,0,0))
+    legend.paste(pil_image_iso, (0, 0))
+    legend.paste(pil_image_x, (window.GetSize()[0], 0))
+    legend.paste(pil_image_y, (0, window.GetSize()[1]))
+    legend.paste(pil_image_z, (window.GetSize()[0],window.GetSize()[1]))
+    # image descriptions
+    draw = ImageDraw.Draw(legend)
+    draw.text((10,10), "ISO", "magenta",font_size=50)
+    draw.text((window.GetSize()[0]+10,10), "X", "magenta",font_size=50)
+    draw.text((10,window.GetSize()[1]+10), "Y", "magenta",font_size=50)
+    draw.text((window.GetSize()[0]+10,window.GetSize()[1]+10), "Z", "magenta",font_size=50)
+    # image saving
+    legend.save(os.path.join(output_directory,"legend.png"))
